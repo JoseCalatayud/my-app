f335417e6553b350da2a9bbbf6b270fe38795654
curso05 <notiene@notiene.es>
2024-11-04 10:51:30 +0100

    Ultimas modificaciones antes de proyecto 2

--- src/main/java/com/mycompany/app/Juan.java
+++ src/main/java/com/mycompany/app/Juan.java
@@ -7,12 +7,62 @@ public class Juan extends Object {
     private int edad;
     private String ciudad;
     private short numero;
-    private char cahracter;
+    private char caracter;
     public Juan() {
     }
 
     public Juan(String nombre) {
         this.nombre = nombre;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public short getNumero() {
+        return numero;
+    }
+
+    public void setNumero(short numero) {
+        this.numero = numero;
+    }
+
+    public char getCaracter() {
+        return caracter;
+    }
+
+    public void setCaracter(char caracter) {
+        this.caracter = caracter;
+    }
+
+    
 
 }
---
+++ src/test/java/com/mycompany/app/CocheTest.java
@@ -0,0 +1,45 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+
+public class CocheTest {
+    @Test
+    void testGetMatricula() {
+
+    }
+
+    @Test
+    void testGetNumeroMarcha() {
+
+    }
+
+    @Test
+    void testGetTipoDeCambio() {
+
+    }
+
+    @Test
+    void testSetMatricula() {
+
+    }
+
+    @Test
+    void testSetNumeroMarcha() {
+
+    }
+
+    @Test
+    void testSetTipoDeCambio() {
+
+    }
+
+    @Test
+    void testSubbajarMarcha() {
+
+    }
+
+    @Test
+    void testSubirMarcha() {
+
+    }
+}
